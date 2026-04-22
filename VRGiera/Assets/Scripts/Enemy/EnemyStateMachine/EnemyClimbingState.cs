65a74855b10bd6df692137082abf5dd6bfd814fb
Dorians <dorianmarekwolny@wp.pl>
2026-04-22 19:44:40 +0200

    Enemy State Machine in progress

---
+++ VRGiera/Assets/Scripts/Enemy/EnemyStateMachine/EnemyClimbingState.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyClimbingState : EnemyBaseState
+{
+    public override void EnterState(EnemyStateManager enemy)
+    {
+    }
+
+
+    public override void UpdateState(EnemyStateManager enemy)
+    {
+    }
+    public override void OnCollisionEnter(EnemyStateManager enemy)
+    {
+        throw new System.NotImplementedException();
+    }
+}
