2e19f198a305a816af1da3743f86d767edf545cd
Dorians <dorianmarekwolny@wp.pl>
2026-05-26 19:14:00 +0200

    EnemyClimbing
    enemy sie wspina trzeba jeszcze poprawic troche ustawienie navmesh link

--- VRGiera/Assets/Scripts/Enemy/EnemyStateMachine/EnemyStateManager.cs
+++ VRGiera/Assets/Scripts/Enemy/EnemyStateMachine/EnemyStateManager.cs
@@ -1,28 +1,53 @@
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyStateManager : MonoBehaviour
 {
-    EnemyBaseState currentState;
-    public EnemyIdleState idleState = new EnemyIdleState();
-    public EnemyWalkingState walkingState = new EnemyWalkingState();
-    public EnemyClimbingState climbingState = new EnemyClimbingState();
+    private EnemyBaseState currentState;
 
+    public readonly EnemyIdleState idleState = new EnemyIdleState();
+    public readonly EnemyWalkingState walkingState = new EnemyWalkingState();
+    public readonly EnemyClimbingState climbingState = new EnemyClimbingState();
+
+    [SerializeField] private Transform playerTarget;
+    [SerializeField] private float stateChangeDelay;
+    [SerializeField] private float lethalDistance;
+    [SerializeField] private float climbSpeed;
+
+    private NavMeshAgent navAgent;
+
+    public Transform PlayerTarget => playerTarget;
+    public float StateChangeDelay => stateChangeDelay;
+    public float LethalDistance => lethalDistance;
+    public float ClimbSpeed => climbSpeed;
+    public NavMeshAgent NavAgent => navAgent;
+
+    private void Awake()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        navAgent.autoTraverseOffMeshLink = false;
+    }
 
     private void Start()
     {
         currentState = idleState;
-
         currentState.EnterState(this);
     }
 
     private void Update()
     {
-        currentState.UpdateState(this);
+        currentState?.UpdateState(this);
     }
 
     public void SwitchState(EnemyBaseState state)
     {
         currentState = state;
-        currentState.EnterState(this);
+        currentState?.EnterState(this);
+    }
+
+    public void ExecutePlayer()
+    {
+        gameObject.SetActive(false);
     }
 }
