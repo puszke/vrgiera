190dfcf18438ac21d8a1eb52ded35cd4f7bc4234
Dorians <dorianmarekwolny@wp.pl>
2026-05-26 19:14:00 +0200

    EnemyClimbing
    enemy sie wspina trzeba jeszcze poprawic troche ustawienie navmesh link

--- VRGiera/Assets/Scripts/Enemy/EnemyStateMachine/EnemyClimbingState.cs
+++ VRGiera/Assets/Scripts/Enemy/EnemyStateMachine/EnemyClimbingState.cs
@@ -1,17 +1,39 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyClimbingState : EnemyBaseState
 {
+    private const float ClimbCompletionThreshold = 1f;
+    private const float InitialClimbProgress = 0f;
+
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private float climbProgress;
+    private float journeyLength;
+
     public override void EnterState(EnemyStateManager enemy)
     {
-    }
+        OffMeshLinkData data = enemy.NavAgent.currentOffMeshLinkData;
+        startPos = enemy.transform.position;
+        endPos = data.endPos + Vector3.up * enemy.NavAgent.baseOffset;
 
+        journeyLength = Vector3.Distance(startPos, endPos);
+        climbProgress = InitialClimbProgress;
+    }
 
     public override void UpdateState(EnemyStateManager enemy)
     {
+        climbProgress += (enemy.ClimbSpeed * Time.deltaTime) / journeyLength;
+        enemy.transform.position = Vector3.Lerp(startPos, endPos, climbProgress);
+
+        if (climbProgress >= ClimbCompletionThreshold)
+        {
+            enemy.NavAgent.CompleteOffMeshLink();
+            enemy.SwitchState(enemy.walkingState);
+        }
     }
+
     public override void OnCollisionEnter(EnemyStateManager enemy)
     {
-        throw new System.NotImplementedException();
     }
 }
